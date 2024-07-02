4168a68b18791b1f168523473daa2f9a3eb0994f
mateen <mateenazfar786@gmail.com>
2024-07-02 10:18:19 +0530

    Applied Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import SignUp from "./screens/SignUp";
 import Layout from "./screens/Layout";
+import PrivateRoutes from "./components/PrivateRoutes";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route element={<PrivateRoutes />}>
+            <Route index element={<Home />} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<SignUp />} />
         </Route>
