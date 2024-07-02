c433b7ce7038c8721d080be85e09f207ed3e219a
mateen <mateenazfar786@gmail.com>
2024-07-02 15:45:05 +0530

    Sign Up completed

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -1,31 +1,79 @@
-import { Navigate } from "react-router-dom";
-import useStore from "../store";
+// import {  useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { account, databases } from "../appwrit";
+import { ID } from "appwrite";
+import { motion } from "framer-motion";
+
+const databaseId = import.meta.env.VITE_TODO_DATABASE_ID;
+const dataCollectionId = import.meta.env.VITE_DATA_COLLECTION_ID;
 
 export default function SignUp() {
-  const userId = useStore((state) => state.userId);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
 
-  if (userId) {
-    return <Navigate to="/" />;
-  }
+  const handleSubmit = () => {
+    const promise = account.create(ID.unique(), email, password);
+    promise.then(
+      function (responses) {
+        console.log(responses);
+        const promise = databases.createDocument(
+          databaseId,
+          dataCollectionId,
+          ID.unique(),
+          {
+            user_id: responses.$id,
+            // user_tasks: [],
+          }
+        );
+
+        promise.then(
+          function (response) {
+            console.log(response);
+            setSuccess(true);
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+  };
   return (
-    <div className="flex items-center justify-center w-full h-full">
+    <div className="flex items-center justify-center w-full h-full relative">
+      {success && (
+        <motion.div
+          className="absolute top-5 right-20 bg-green-400 text-white px-4 rounded-md"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h1 className="text-sm font-bold">Account Created Sucessfully!!</h1>
+          <h1 className="text-sm font-bold">Pleace LOG IN to continue</h1>
+        </motion.div>
+      )}
       <div className="flex flex-col gap-3">
         <h1 className="text-white text-3xl font-bold">Email</h1>
         <input
           type="email"
           className="border-b-2 border-white bg-transparent outline-none text-xl"
-        />
-        <h1 className="text-white text-3xl font-bold">Name</h1>
-        <input
-          type="text"
-          className="border-b-2 border-white bg-transparent outline-none text-xl"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <h1 className="text-white text-3xl font-bold">Password</h1>
         <input
           type="password"
           className="border-b-2 border-white bg-transparent outline-none text-xl"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <div className="flex items-center justify-center  mt-5 cursor-pointer">
+        <div
+          onClick={handleSubmit}
+          className="flex items-center justify-center  mt-5 cursor-pointer"
+        >
           <h3 className="border-2 border-white rounded-full text-white font-semibold px-4 py-1 text-xl hover:text-black">
             Sign Up
           </h3>
